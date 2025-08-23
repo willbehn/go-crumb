a4e641d040facc5cf5c942e92c35747f88e2c43a
William Henrik Behn <behnwilliam@icloud.com>
2025-08-23 23:28:49 +0200

    Add command for recent commands
    Adding command "recent" to show all recent commands with timestamp.

---
+++ cmd/recent.go
@@ -0,0 +1,85 @@
+package cmd
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"time"
+	"willbehn/ht/models"
+
+	"github.com/spf13/cobra"
+)
+
+var recentCmd = &cobra.Command{
+	Use:   "recent",
+	Short: "A brief description of your command",
+	Long:  `Fiks senere`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		path := os.Getenv("HT_DB")
+		if path == "" {
+			return fmt.Errorf("invalid path to db")
+		}
+
+		db, err := sql.Open("sqlite", path)
+
+		if err != nil {
+			return err
+		}
+
+		defer db.Close()
+
+		tx, err := db.BeginTx(cmd.Context(), nil)
+
+		if err != nil {
+			return err
+		}
+
+		rows, err := tx.Query(`
+			SELECT cmd, shell, dir, repo, branch, ts, exit_code, duration_ms  
+			FROM commands
+			ORDER BY ts`)
+
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+
+		var results []models.CmdEvent
+
+		for rows.Next() {
+			var ev models.CmdEvent
+
+			if err := rows.Scan(
+				&ev.Cmd,
+				&ev.Shell,
+				&ev.Dir,
+				&ev.Repo,
+				&ev.Branch,
+				&ev.TS,
+				&ev.Exit,
+				&ev.Dur,
+			); err != nil {
+				return err
+			}
+			results = append(results, ev)
+		}
+
+		if err := tx.Commit(); err != nil {
+			return err
+		}
+
+		for _, ev := range results {
+
+			ts := int64(ev.TS)
+			t := time.Unix(ts, 0)
+			fmt.Println(t, ev.Cmd)
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(recentCmd)
+
+}
