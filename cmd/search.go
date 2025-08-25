6aabf991f002e68ed39b140431038362e556967f
William Henrik Behn <behnwilliam@icloud.com>
2025-08-25 19:46:35 +0200

    Legg til foerste iterasjon av search

---
+++ cmd/search.go
@@ -0,0 +1,79 @@
+package cmd
+
+import (
+	"fmt"
+	"strings"
+	"time"
+	"willbehn/ht/internal"
+	"willbehn/ht/models"
+
+	"github.com/spf13/cobra"
+)
+
+var searchCmd = &cobra.Command{
+	Use:   "search",
+	Short: "A brief description of your command",
+	Long:  `Fiks senere`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		db, err := internal.OpenDB()
+
+		if err != nil {
+			return err
+		}
+		defer db.Close()
+
+		var conditions []string
+		var parameters []interface{}
+
+		query := `SELECT cmd, shell, dir, repo, branch, ts, exit_code, duration_ms  
+		FROM commands `
+
+		for _, arg := range args {
+			conditions = append(conditions, "cmd LIKE ?")
+			parameters = append(parameters, "%"+arg+"%")
+		}
+
+		if len(conditions) > 0 {
+			query += "WHERE " + strings.Join(conditions, "AND")
+		}
+
+		rows, err := db.Query(query, parameters...)
+
+		if err != nil {
+			return err
+		}
+
+		var results []models.CmdEvent
+
+		for rows.Next() {
+			var ev models.CmdEvent
+
+			if err := rows.Scan(
+				&ev.Cmd,
+				&ev.Shell,
+				&ev.Dir,
+				&ev.Repo,
+				&ev.Branch,
+				&ev.TS,
+				&ev.Exit,
+				&ev.Dur,
+			); err != nil {
+				return err
+			}
+			results = append(results, ev)
+		}
+
+		for _, ev := range results {
+
+			ts := int64(ev.TS)
+			t := time.Unix(ts, 0).Local()
+			fmt.Printf("(%s) %s\n", t.Format("2006-01-02 15:04:05"), ev.Cmd)
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(searchCmd)
+}
