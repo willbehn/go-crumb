2c2249fb80d2723bcd98b55c0becae09ccbc0971
William Henrik Behn <behnwilliam@icloud.com>
2025-08-26 15:41:59 +0200

    Foerste utkast for behandling av flagg i record funksjonen

--- cmd/record.go
+++ cmd/record.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	"encoding/json"
-	"os"
 	"willbehn/ht/internal"
 	"willbehn/ht/models"
 
@@ -10,17 +8,44 @@ import (
 	_ "modernc.org/sqlite"
 )
 
-func init() { rootCmd.AddCommand(recordCmd) }
+var (
+	recCmdStr string
+	recShell  string
+	recDir    string
+	recRepo   string
+	recBranch string
+	recTS     int64
+	recExit   int
+	recDur    int64
+)
+
+func init() {
+	f := recordCmd.Flags()
+	f.StringVar(&recCmdStr, "cmd", "", "full command line")
+	f.StringVar(&recShell, "shell", "zsh", "shell name (zsh/bash/fish)")
+	f.StringVar(&recDir, "dir", "", "working directory")
+	f.StringVar(&recRepo, "repo", "", "git repo (optional)")
+	f.StringVar(&recBranch, "branch", "", "git branch (optional)")
+	f.Int64Var(&recTS, "ts", 0, "unix timestamp (seconds)")
+	f.IntVar(&recExit, "exit", 0, "exit code")
+	f.Int64Var(&recDur, "dur", 0, "duration ms (optional)")
+	rootCmd.AddCommand(recordCmd)
+}
 
 var recordCmd = &cobra.Command{
 	Use:   "record",
 	Short: "fiks senere",
 	RunE: func(cmd *cobra.Command, args []string) error {
 
-		var ev models.CmdEvent
-
-		if err := json.NewDecoder(os.Stdin).Decode(&ev); err != nil {
-			return err
+		ev := models.CmdEvent{
+			Cmd:    recCmdStr,
+			Shell:  recShell,
+			Dir:    recDir,
+			Repo:   strptr(recRepo),
+			Branch: strptr(recBranch),
+			TS:     recTS,
+			Exit:   recExit,
+			Dur:    recDur,
 		}
 
 		db, err := internal.OpenDB()
@@ -47,4 +72,11 @@ var recordCmd = &cobra.Command{
 
 		return tx.Commit()
 	},
+}
+
+func strptr(s string) *string {
+	if s == "" {
+		return nil
+	}
+	return &s
 }
